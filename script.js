d419ae62edfb3b84b56617a44b310b0bfc21dae0
KarinaNalepa <karina.nalepa@escola.pr.gov.br>
2026-06-15 10:37:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.textContent = "O agro está presente em tudo: do campo até a sua mesa. Valorize quem produz!";
+}
